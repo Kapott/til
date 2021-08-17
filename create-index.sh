ef6f93dfa1b7ff9dc7b5585e1393dc5f718e6833
kapott <kapott@users.noreply.github.com>
2021-08-17 16:15:47 +0200

    Syntax, readability etc.

--- create-index.sh
+++ create-index.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# This script goes through each directory, searches the file for the 
+# header tag (single #), and uses that as the link's title.
+
 OUTFILE="README.md"
 rm "$OUTFILE" 2> /dev/null && touch "$OUTFILE"
-
-# rg --vimgrep -e '^# ' . | awk -F: '{ printf "- [%s](%s)  \n", substr($4,3), substr($1,3) }' | sort > $OUTFILE
 
 ls -d */ | {
   while read -r line; 
     do 
-      fmtline=$(echo $line | tr '[:lower:]' '[:upper:]')
+      fmtline=$(echo $line | tr '[:lower:]' '[:upper:]') 
       echo "# ${fmtline%?}" >> $OUTFILE
-      rg --vimgrep -e '^# ' ./${line}/* | awk -F: '{ printf "- [%s](%s)  \n", substr($4,3), substr($1,3) }' | sort >> $OUTFILE
+      rg --vimgrep -e '^# ' ./${line}/* |
+        awk -F: '{ printf "- [%s](%s)  \n", substr($4,3), substr($1,3) }' |
+        sort >> $OUTFILE
     done;
 }
