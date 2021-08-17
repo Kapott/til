d9d572d25f8c92730c6bf69b9e11d56e628645f1
kapott <kapott@users.noreply.github.com>
2021-08-17 16:53:52 +0200

    Fix bug where comments and lower headers were used as links.

--- create-index.sh
+++ create-index.sh
@@ -9,10 +9,12 @@ rm "$OUTFILE" 2> /dev/null && touch "$OUTFILE"
 ls -d */ | {
   while read -r line; 
     do 
-      fmtline=$(echo $line | tr '[:lower:]' '[:upper:]') 
+      fmtline=$(echo $line | tr '-' ' ' | tr '[:lower:]' '[:upper:]') 
       echo "# ${fmtline%?}" >> $OUTFILE
       rg --vimgrep -e '^# ' ./${line}/* |
-        awk -F: '{ printf "- [%s](%s)  \n", substr($4,3), substr($1,3) }' |
+        awk -F: '$2 == "1" { printf "- [%s](%s)  \n", substr($4,3), substr($1,3) }' |
         sort >> $OUTFILE
     done;
 }
+
+git commit "$OUTFILE" -m "Automatic indexing commit."
